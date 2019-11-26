cf0aeba6456b73c83134708349adf3439bdde34f
kaelsilva <kael.leak1998@gmail.com>
2019-11-26 12:12:55 -0300

    Adicionando dependência mongoose e conexão do mongodb. #15

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-//Definindo constante da dependência express (o retorno é uma função)
+//Definindo constante das dependências (o retorno é uma função)
 const express = require('express');
+const mongoose = require('mongoose');
 
 //Definindo constante e executando função express
 const app = express();
+
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {useNewUrlParser: true}
+);
+
+//Definindo rotas a serem acessadas pelo usuário
+app.get('/', (req, res) => {
+
+});
 
 //Indicando para a aplicação ouvir a porta 3001 do navegador (http://localhost:3001)
 app.listen(3001);
