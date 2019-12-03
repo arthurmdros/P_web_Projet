2f5426044ed9520c680cffd0d89924606cf975b1
kaelsilva <kael.leak1998@gmail.com>
2019-12-03 12:27:22 -0300

    Alterando o header principal da aplicação.

--- kaelsilva/reactapp/src/components/Header/index.js
+++ kaelsilva/reactapp/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
-    <header id="main-header">Clientes</header>
+    <header id="main-header">Loja de roupas</header>
 );
 
 export default Header;
