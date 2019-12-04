c792f1cc391e522785b7a35cc2103db01336b235
cadu_brito <clodoaldo.brito45@hotmail.com>
2019-12-04 18:50:36 -0300

    update listfuncionario

--- cadu-brito/node-api/server.js
+++ cadu-brito/node-api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 //iniciando do banco de dados
-mongoose.connect('mongodb://localhost:27017/nodeapi',{ useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useUnifiedTopology: true, useNewUrlParser: true});
 
 requireDir('./src/models');
 
