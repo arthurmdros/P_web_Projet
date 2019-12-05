55ec35930eac72e8f81ba26fa5c11a3672f2d7ec
Ewerton Silva <ewertoncom297@gmail.com>
2019-12-05 01:16:27 -0600

    Conectando ao banco de dados

--- Ewertonslv/node-api/server.js
+++ Ewertonslv/node-api/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//iniciando app
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("oi ewerton")
+//iniciando DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+     { useNewUrlParser: true });
 
+
+//primeira rota
+app.get('/', (req, res) => {
+    res.send("oi ewertoon")
 });
 
 app.listen(3001);
