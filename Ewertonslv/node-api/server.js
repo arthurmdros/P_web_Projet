78a712b5730991e8b34b2f463a614255082ed8c7
Ewerton Silva <ewertoncom297@gmail.com>
2019-12-05 02:05:47 -0600

    Configurando server.js

--- Ewertonslv/node-api/server.js
+++ Ewertonslv/node-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //iniciando app
 const app = express();
@@ -7,12 +8,12 @@ const app = express();
 //iniciando DB
 mongoose.connect(
     'mongodb://localhost:27017/nodeapi',
-     { useNewUrlParser: true });
+     { useNewUrlParser: true }
+);
+requireDir('./src/models');
 
 
-//primeira rota
-app.get('/', (req, res) => {
-    res.send("oi ewertoon")
-});
+//Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
