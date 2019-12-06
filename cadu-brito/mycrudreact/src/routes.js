afaf2a114a3f77d351ece29ae2777385d2deb437
cadu_brito <clodoaldo.brito45@hotmail.com>
2019-12-06 01:41:53 -0300

    adicionando as rotas e modificando #13

--- cadu-brito/mycrudreact/src/routes.js
+++ cadu-brito/mycrudreact/src/routes.js
@@ -6,19 +6,20 @@ import Customer from './pages/customer';
 import New_Customer from './pages/new_customer';
 import User from './pages/user';
 import Mainfuncionarios from './pages/Mainfuncionarios';
-import Novo_Funcionario from './pages/novofuncionario';
+import NovoFuncionario from './pages/novofuncionario';
 
 const Routes = () =>( 
 <BrowserRouter>
     <Switch>
-        <Route path="/user" component={User} />
-        <Route exact path="/customers" component={Main} />
-        <Route path="/customers/:id" component={Customer} />
-        <Route path="/new_customer" component={New_Customer} />
+
+            <Route exact path="/customers" component={Main} />
+            <Route path="/customers/:id" component={Customer} />
+            <Route path="/new_customer" component={New_Customer} />
+            <Route path="/user" component={User} />
 
         <Route exact path="/funcionarios" component={Mainfuncionarios} />
         <Route path="/funcionarios/:id" component={Funcionario}/>
-        <Route path="/novofuncionario" component={Novo_Funcionario} />
+        <Route path="/novofuncionario" component={NovoFuncionario} />
     </Switch>
 </BrowserRouter>
 );
